0be918a15e9e8708bba0f897191061a863fcf80e
Flavia Villani <flavia.villani14@gmail.com>
2020-04-30 10:29:00 +0200

    Update Metadata.py

--- Metadata.py
+++ Metadata.py
@@ -2,7 +2,7 @@ import os
 
 ms_command = ''
 
-with open('/home/flavia/Desktop/GfatoAlleleFreq/tree2pop.ms','r') as f:
+with open('tree2pop.ms','r') as f:              #input is: output of ms as tree before obtained seq from seqgene
 	ms_command = f.readline().strip('\n')
 
 print('ms_command: '+ ms_command)     
@@ -16,19 +16,19 @@ if '-I' in ms_command:
 	ind = int(info_list[2])
 	
 	#for indiv in range (1, (NumHaplo + ind)+1):
-		#print('-individual',indiv)
+		#print('-individual',indiv)              
 	
 
 	metadata = ' '
-		
-
+	
 	for pop in range(1, POP+1):
 		for haplo in range(1,NumHaplo+1):
 			metadata = ('POP',pop,'individual:', int((haplo - 1) / 2), 'haplo', haplo) 
 			print(metadata)
 
 
-			#TODO: add version of script that there is write of a file metadata tab separated
+	#TODO (IMPORTANT): add version of script that there is write of a file metadata tab separated 
+	#to give as second input of script that calculate AF  
 
 			
 '''
