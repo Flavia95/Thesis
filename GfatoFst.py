3c53c2708181b1e83adf7aee33961d8fe6d1f97a
Flavia Villani <flavia.villani14@gmail.com>
2020-05-15 09:28:50 +0200

    Update and rename GfatoFst to GfatoFst.py

--- GfatoFst.py
+++ GfatoFst.py
@@ -3,19 +3,21 @@ import matplotlib.pyplot as pyplot
 
 mean_fst_list = []
 
-for num_rep in range(1,2):                                 #usare con tanti replicati
+for num_rep in range(1,2):                                 #range replicate
 	f1 = open('allelfr_rep{}.pop1.tsv'.format(num_rep))
 	f2 = open('allelfr_rep{}.pop2.tsv'.format(num_rep))
 
 fst_list = []
-#f1.readline() # Skip header
-#f2.readline() # Skip header
+#f1.readline() # Skip header or first line
+#f2.readline() 
+
 for (line1, line2) in zip(f1, f2):
-    freq_pop1 = line1.strip('\n').split('\t')[-1]   #ultima colonna 
-    freq_pop2 = line2.strip('\n').split('\t')[-1]  #ultima colonna
+    freq_pop1 = line1.strip('\n').split('\t')[-1]   #last column
+    freq_pop2 = line2.strip('\n').split('\t')[-1]  
 
     #print(freq_pop1)
     #print(freq_pop2)
+    
     freq0_pop1 = float(freq_pop1)
     freq0_pop2=float(freq_pop2)
     mean = statistics.mean([freq0_pop1, freq0_pop2])
